f70ea5524f9beb526024ed033f425ab4fb955b0c
sumeyraltas <sumeyraltas@gmail.com>
2023-11-07 15:28:53 +0300

    Initial Commit

---
+++ how-many-minutes-did-you-study/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  how-many-minutes-did-you-study
+//
+//  Created by Sumeyra Altas on 7.11.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ how-many-minutes-did-you-study/how_many_minutes_did_you_studyApp.swift
@@ -0,0 +1,17 @@
+//
+//  how_many_minutes_did_you_studyApp.swift
+//  how-many-minutes-did-you-study
+//
+//  Created by Sumeyra Altas on 7.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct how_many_minutes_did_you_studyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
